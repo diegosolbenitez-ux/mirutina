8c6d3795f0fccfba38999fb928aa695ae18b6096
diegosolbenitez-ux <dbenitezproyectos@gmail.com>
2026-03-04 09:15:15 -0600

    Increase gap in CreateRoutine component layout

--- src/pages/CreateRoutine.tsx
+++ src/pages/CreateRoutine.tsx
@@ -496,7 +496,7 @@ const containerStyle: React.CSSProperties = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  gap: 30,
+  gap: 60,
   paddingTop: 40,
   fontFamily: "monospace"
 }
